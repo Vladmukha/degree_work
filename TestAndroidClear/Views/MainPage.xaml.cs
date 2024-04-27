da14d7d36081af43cec3424ad689e6b78b86a1ce
Vladislav <v_mukha1@mail.ru>
2024-04-27 06:06:52 +0500

    Добавлены комментарии к коду а также, анимация увеличения высоты элемента Frame по нажатию его кнопки

--- TestAndroidClear/Views/MainPage.xaml.cs
+++ TestAndroidClear/Views/MainPage.xaml.cs
@@ -25,24 +25,35 @@ namespace TestAndroidClear.Views
         Button prbutton;
         Grid grid;
         CollectionView collectionView;
+        Button headBTN;
+        
         public MainPage()
         {
             InitializeComponent();
 
-
+            // Параметры подключения к базе данных SQL Server
             string srvrdbname = "Recipes";
             string srvrname = "192.168.2.30";
             string srvrusername = "vmukha";
-            string srvrpasswoed = "123456";
-            string sqlconn = $"Data Source={srvrname};Initial Catalog={srvrdbname}; User Id={srvrusername};Password={srvrpasswoed}";
+            string srvrpassword = "123456";
+
+            // Формирование строки подключения к SQL Server
+            string sqlconn = $"Data Source={srvrname};Initial Catalog={srvrdbname};User Id={srvrusername};Password={srvrpassword}";
+
+            // Создание объекта SqlConnection для установления соединения с базой данных
             sqlConnection = new SqlConnection(sqlconn);
+
+            // Открытие соединения с базой данных
             sqlConnection.Open();
 
+            // Создание кнопки "Continue" и добавление ее в StackLayout
             Button button = new Button()
             {
                 Text = "Continue",
             };
             stack.Children.Add(button);
+
+            // Установка обработчика события Clicked для кнопки
             button.Clicked += Button_clickAsync;
         }
 
@@ -50,12 +61,15 @@ namespace TestAndroidClear.Views
         {
             try
             {
+                // Создаем список категорий
                 List<Categories> categories = new List<Categories>();
 
-                string querryString = "Select * from Recipes.dbo.Catigories";
+                // Запрос к базе данных для получения списка категорий
+                string querryString = "Select * from Recipes.dbo.Categories";
                 SqlCommand command = new SqlCommand(querryString, sqlConnection);
                 SqlDataReader reader = command.ExecuteReader();
 
+                // Читаем результаты запроса и добавляем категории в список
                 while (reader.Read())
                 {
                     categories.Add(new Categories
@@ -66,14 +80,19 @@ namespace TestAndroidClear.Views
                     });
                 }
                 reader.Close();
+
+                // Обработка каждой категории
                 for (int i = 0; i < categories.Count; i++)
                 {
+                    // Создаем список продуктов для текущей категории
                     List<Products> products = new List<Products>();
 
+                    // Запрос к базе данных для получения списка продуктов текущей категории
                     string querryString1 = "Select * from Recipes.dbo." + categories[i].CategoryATBName;
                     SqlCommand command1 = new SqlCommand(querryString1, sqlConnection);
                     SqlDataReader reader1 = command1.ExecuteReader();
 
+                    // Читаем результаты запроса и добавляем продукты в список
                     while (reader1.Read())
                     {
                         products.Add(new Products
@@ -84,41 +103,56 @@ namespace TestAndroidClear.Views
                     }
                     reader1.Close();
 
+                    // Создаем новый Frame для текущей категории
                     frame = new Frame()
                     {
-                         
+                        ClassId = "" + i,
                         HeightRequest = 140,
                         CornerRadius = 16,
                         Margin = new Thickness(0, 5, 0, 5),
                     };
+
+                    // Добавляем Frame в StackLayout
                     stack.Children.Add(frame);
+
+                    // Создаем макет для элементов CollectionView
                     var gridLayout = new GridItemsLayout(orientation: ItemsLayoutOrientation.Vertical);
                     gridLayout.Span = 2;
+
+                    // Создаем и настраиваем CollectionView для отображения продуктов
                     collectionView = new CollectionView();
                     collectionView.ItemsSource = products;
                     collectionView.ItemsLayout = gridLayout;
                     collectionView.ItemTemplate = new DataTemplate(() =>
                     {
+                        // Создаем кнопку для отображения продукта
                         Button PRButton = new Button();
 
+                        // Привязываем текст кнопки к свойству IngName объекта Products
                         PRButton.SetBinding(Button.TextProperty, "IngName");
                         PRButton.FontSize = 12;
 
                         return PRButton;
                     });
 
+                    // Получаем название категории для использования в заголовке кнопки
                     string header = Convert.ToString(categories[i].CategoryTBName);
 
-                    var headBTN = new Button
+                    // Создаем кнопку с заголовком категории
+                    headBTN = new Button
                     {
+                        ClassId = "" + i,
                         Text = header,
                         TextColor = Color.Black,
                         FontAttributes = FontAttributes.Bold,
                         FontSize = 16,
                         BackgroundColor = Color.White,
                     };
+
+                    // Устанавливаем обработчик события нажатия кнопки
                     headBTN.Clicked += HeadBTNClick;
 
+                    // Создаем StackLayout для размещения кнопки и CollectionView
                     StackLayout q1 = new StackLayout()
                     {
                         Children =
@@ -128,42 +162,77 @@ namespace TestAndroidClear.Views
                         }
                     };
 
-
+                    // Устанавливаем содержимое Frame
                     frame.Content = q1;
-
                 }
             }
             catch (Exception ex)
             {
+                // В случае исключения выводим сообщение об ошибке
                 await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "Ok");
                 throw;
             }
         }
 
 
-        private void HeadBTNClick (object sender, EventArgs e)
+        private async void HeadBTNClick(object sender, EventArgs e)
         {
-            if (frame.Height < 100)
+            // Получаем ссылку на кнопку, которая была нажата
+            var button = (Button)sender;
+
+            // Получаем ClassId кнопки, чтобы идентифицировать соответствующий Frame
+            var btnclass = button.ClassId;
+
+            // Находим Frame, соответствующий нажатой кнопке
+            var fr = stack.Children.FirstOrDefault(child => child is Frame && ((Frame)child).ClassId == btnclass) as Frame;
+
+            // Проверяем высоту Frame и выполняем соответствующее действие
+            if (fr.Height > 181)
             {
-                frame.HeightRequest = Double.NaN;
+                // Если высота больше 181, вызываем метод сворачивания Frame
+                await CollapseFrame(fr);
             }
-            else if (frame.Height > 140)
+            else if (fr.Height <= 180)
             {
-                frame.HeightRequest = 140;
+                // Если высота меньше или равна 180, вызываем метод разворачивания Frame
+                await ExpandFrame(fr);
             }
         }
-
-        // !
-        private int CalculateSpanCount()
+        public async Task ExpandFrame(Frame frame, int durationMilliseconds = 250, uint animationLength = 16)
         {
-            double screenWidth = Xamarin.Forms.Application.Current.MainPage.Width;
-            double itemWidth = 100; // Ширина элемента (примерное значение)
-            int minSpan = 1; // Минимальное количество элементов в строке
+            // Определяем начальную и конечную высоту
+            double startingHeight = frame.Height;
+            double targetHeight = 600;
 
-            // Вычисление количества элементов в строке (можно настроить логику по желанию)
-            int spanCount = (int)(screenWidth / itemWidth); // Пример: один элемент в строке за 100 единиц ширины
+            // Устанавливаем начальную высоту перед началом анимации
+            frame.HeightRequest = startingHeight;
 
-            return Math.Max(spanCount, minSpan); // Возвращаем вычисленное значение, но не менее минимального
+            // Анимация разворачивания
+            while (frame.HeightRequest < targetHeight)
+            {
+                frame.HeightRequest += animationLength;
+                await Task.Delay(durationMilliseconds / (int)((targetHeight - startingHeight) / animationLength));
+            }
+
+            // Убеждаемся, что окончательная высота равна целевой высоте
+            frame.HeightRequest = targetHeight;
+        }
+
+        public async Task CollapseFrame(Frame frame, int durationMilliseconds = 250, uint animationLength = 16)
+        {
+            // Определяем начальную и конечную высоту
+            double startingHeight = frame.Height;
+            double targetHeight = 180;
+
+            // Анимация сворачивания
+            while (frame.HeightRequest > targetHeight)
+            {
+                frame.HeightRequest -= animationLength;
+                await Task.Delay(durationMilliseconds / (int)((startingHeight - targetHeight) / animationLength));
+            }
+
+            // Убеждаемся, что окончательная высота равна целевой высоте
+            frame.HeightRequest = targetHeight;
         }
     }
 }
